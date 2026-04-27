8e04a00aca8684f07871470da202118fac0d5cf7
Carlos <carlosrizzetti@gmail.com>
2026-04-27 18:58:55 -0300

    primer commit

--- src/test/java/com/tallerwebi/dominio/ServicioMotorTest.java
+++ src/test/java/com/tallerwebi/dominio/ServicioMotorTest.java
@@ -1,34 +1,49 @@
 package com.tallerwebi.dominio;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 public class ServicioMotorTest {
-private ServicioMotor servicioMotor;
-@BeforeEach
-public void init() {
-    this.servicioMotor=new ServicioMotorImpl();
-}
+    private ServicioMotor servicioMotor;
+
+    @BeforeEach
+    public void init() {
+        this.servicioMotor = new ServicioMotorImpl();
+    }
 
     @Test
     public void deberiaCrearUnMotor() {
-        
-        Motor motor=new Motor();
-        Boolean creado=servicioMotor.crear(motor);
-        assertThat(creado,is ( true));
+        Motor motor = dadoQueTengoUnMotor();
+        Boolean creado = cuandoCreoUnMotor(motor);
+        elResultadoEsVerdadero(creado);
     }
+
     @Test
     public void deberiaModificarElNombreDeUnMotor() {
-                      
-            Boolean modificado=servicioMotor.modificarNombre(1, "Motor modificado");
-            assertThat(modificado,is ( true));
+        // given(dado) / when(cuando) / then(entonces)
+        Boolean modificado = servicioMotor.modificarNombre(1, "Motor modificado");
+        elResultadoEsVerdadero(modificado);
     }
- /*    @Test
-    public void noDeberiaModificarElNombreDeUnMotorSiElIdNoExiste() {
-            ServicioMotor servicioMotor=new ServicioMotorImpl();           
-            Boolean modificado=servicioMotor.modificarNombre(1, "Motor modificado");
-            assertThat(modificado,is ( true));
-    } */
+
+    private Motor dadoQueTengoUnMotor() {
+        return new Motor();
+    }
+    private Boolean cuandoCreoUnMotor(Motor motor) {
+        return servicioMotor.crear(motor);
+    }
+
+    private void elResultadoEsVerdadero(Boolean resultado) {
+        assertThat(resultado, is(true));
+    }
+    /*
+     * @Test
+     * public void noDeberiaModificarElNombreDeUnMotorSiElIdNoExiste() {
+     * ServicioMotor servicioMotor=new ServicioMotorImpl();
+     * Boolean modificado=servicioMotor.modificarNombre(1, "Motor modificado");
+     * assertThat(modificado,is ( true));
+     * }
+     */
 }
-    
